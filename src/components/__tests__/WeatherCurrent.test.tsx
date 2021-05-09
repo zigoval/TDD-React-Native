c0fceac10337a3e4b3bba565b5b604cdd8efff6e
Zigoval <rouby.v@hotmail.com>
2021-05-09 09:07:55 +0200

    ft/localisation

--- src/components/__tests__/WeatherCurrent.test.tsx
+++ src/components/__tests__/WeatherCurrent.test.tsx
@@ -7,4 +7,8 @@ describe('WeatherCurrent', () => {
     const wrapper = render(<WeatherCurrent />);
     wrapper.getByTestId('weather-current');
   });
+
+  test('Should navigate to Weather screen with location', () => {
+    throw new Error('Test not implemented');
+  });
 });
