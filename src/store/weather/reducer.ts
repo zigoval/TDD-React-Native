ba21ce99d43b1d3cfd8b5917b5d4904c40ed84e5
Zigoval <rouby.v@hotmail.com>
2021-05-11 17:26:07 +0200

    Set up weather reducer"

---
+++ src/store/weather/reducer.ts
@@ -0,0 +1,41 @@
+import Status from '../../types/Status';
+import { nullWeather } from '../../types/Weather';
+import { Actions, ActionsTypes } from './action';
+
+export type State = {};
+
+const initialState: State = {
+  weather: nullWeather,
+  status: Status.START,
+  error: '',
+};
+
+export default function (
+  state = initialState,
+  action: ActionsTypes | { type: '@@INIT' },
+): State {
+  switch (action.type) {
+    case Actions.START:
+      return {
+        ...state,
+        status: Status.LOADING,
+      };
+    case Actions.SUCCESS:
+      return {
+        ...state,
+        status: Status.SUCCESS,
+        weather: action.payload.weather,
+      };
+    case Actions.FAILURE:
+      return {
+        ...state,
+        status: Status.FAILURE,
+        error: action.payload.error,
+      };
+
+    case Actions.RESET:
+      return initialState;
+    default:
+      return state;
+  }
+}
